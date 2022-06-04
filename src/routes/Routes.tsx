017e4d8f9f78e6ba84a974e07f94d8e6ad7a83ac
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2022-06-04 09:50:54 +0300

    sidebar

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -5,7 +5,13 @@ const RoutesFile = () => {
     const LoginScreen = lazy(() => import("../screens/Login/Login"))
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={
+            <div className="flex justify-center items-center mt-3">
+                <div className="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-blue-600" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        }>
 
             <Router>
                 <Routes>
