976dd68980ed1ae7ac592680d519ed1d6a5a33d7
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2023-09-19 13:46:55 +0300

    update on the UI

--- src/screens/Products/UpdateProductDetails.tsx
+++ src/screens/Products/UpdateProductDetails.tsx
@@ -327,15 +327,9 @@ const UpdateProductDetails = ({ id }: any) => {
               </div>
 
               <div className="text-center mt-3">
-                {isUpdating ? (
-                  <button className="btn btn-success btn-md" disabled>
-                    Updating
+                <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" disabled ={isUpdating?true:false}>
+                    {isUpdating?"updating":"Update Product"}
                   </button>
-                ) : (
-                  <button className="btn btn-success btn-md">
-                    Update Product
-                  </button>
-                )}
               </div>
             </form>
           </Card>
--- src/screens/Sales/AddSale.tsx
+++ src/screens/Sales/AddSale.tsx
@@ -318,7 +318,7 @@ const AddSale = ({ id }: any) => {
           <div className="text-center mt-3">
             <button
               type="submit"
-              className="btn btn-success btn-md"
+              className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
               disabled={isLoading ? true : false}
             >
               {' '}
