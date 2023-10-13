39e15b6ff4cfece0056e2b76c089f9c32c643dab
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2023-10-13 12:31:48 +0300

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,46 @@ import react from '@vitejs/plugin-react';
 import {VitePWA, VitePWAOptions} from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 
-
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+  manifest: {
+    name: 'BizMkononi',
+    short_name: 'BizMkononi',
+    description: 'An app for your business insights.',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+      {
+        src: '/maskable_icon.png',
+        sizes: '225x225',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    theme_color: '#1B262C',
+    background_color: '#fff',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+};
 export default defineConfig({
   base: './',
-  plugins: [react(), ],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });
